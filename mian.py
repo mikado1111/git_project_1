5f996711896e1027d78ebce8de91ddb9a4754fa5
Kirill_Abdullin <kirya.abdullin@yandex.ru>
2024-01-12 13:21:37 +1100

    Add superfunction

--- mian.py
+++ mian.py
@@ -1,3 +1,6 @@
+def my_superfunction():
+    print('What an awesome print!')
+
 def main():
     print('My first git program')
     print("second line")
@@ -5,6 +8,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
